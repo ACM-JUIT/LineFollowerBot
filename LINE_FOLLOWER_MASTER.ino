03297191d6550774fca24a548de90c4983ee1320
PIYUSH KUMAR SINGH <36163617+PIYUSH-KUMAR-SINGH@users.noreply.github.com>
2018-03-04 22:51:26 +0530

    Add files via upload

---
+++ LINE_FOLLOWER_MASTER.ino
@@ -0,0 +1,68 @@
+
+/*------ Arduino Line Follower Code----- */
+/*-------definning Inputs------*/
+
+
+#include<Wire.h>
+int x ;
+#define LS 2      // left sensor
+#define RS 3      // right sensor
+int buzz=4;
+int right=10;
+int left=11;
+int rear=12;
+
+
+void setup()
+{
+   Wire.begin();
+  Serial.begin(9600);
+  pinMode(LS, INPUT);
+  pinMode(RS, INPUT);
+  pinMode(buzz,OUTPUT);
+  pinMode(rear,OUTPUT);
+  pinMode(right,OUTPUT);
+  pinMode(left,OUTPUT);
+ 
+}
+
+void loop()
+{
+
+  Wire.beginTransmission(9);
+  
+  if(digitalRead(LS) && digitalRead(RS))     // Move Forward
+  {
+      x=1;
+    
+  }
+  
+  if(!(digitalRead(LS)) && digitalRead(RS))     // Turn right
+  {
+   x=2;
+   digitalWrite(right,HIGH);
+   //tone(buzz,3000,50);
+   delay(100);
+   digitalWrite(right,LOW);
+}
+  
+  if(digitalRead(LS) && !(digitalRead(RS)))     // turn left
+  {
+    x=3;
+    digitalWrite(left,HIGH);
+   //tone(buzz,3000,50);
+   delay(100);
+   digitalWrite(left,LOW);
+  }
+  
+  if(!(digitalRead(LS)) && !(digitalRead(RS)))     // stop
+  {
+    x=4;
+    digitalWrite(rear,HIGH);
+    delay(10);
+    digitalWrite(rear,LOW);
+  }
+  Wire.write(x);              // sends x 
+  Wire.endTransmission();    // stop transmitting
+  
+}
---
+++ LINE_FOLLWER_SLAVE.ino
@@ -0,0 +1,58 @@
+#include <Wire.h>
+#include <AFMotor.h>
+AF_DCMotor motor1(1);
+AF_DCMotor motor2(2);
+AF_DCMotor motor3(3);
+AF_DCMotor motor4(4);
+int x= 1 ;
+void setup() {
+
+  // Start the I2C Bus as Slave on address 9
+  Wire.begin(9);
+  // Attach a function to trigger when something is received.
+  Wire.onReceive(receiveEvent);
+  motor1.setSpeed(255);
+motor2.setSpeed(255);
+motor3.setSpeed(255);
+motor4.setSpeed(255);
+}
+void receiveEvent(int bytes) {
+  x = Wire.read();    // read one character from the I2C
+}
+void loop() {
+  //If value received is 0 blink LED for 200 ms
+  if (x == 1) {
+    motor1.run(FORWARD);
+motor2.run(FORWARD);
+motor3.run(FORWARD);
+motor4.run(FORWARD);
+  }
+  //If value received is 3 blink LED for 400 ms
+  
+
+
+  if (x == 2)
+  { motor1.run(BACKWARD);
+motor2.run(FORWARD);
+motor3.run(BACKWARD);
+motor4.run(FORWARD);
+  }
+
+
+  if (x == 3)
+  { motor1.run(FORWARD);
+motor2.run(BACKWARD);
+motor3.run(FORWARD);
+motor4.run(BACKWARD);
+  }
+
+
+
+  if (x == 4)
+  {
+    motor1.run(RELEASE);
+motor2.run(RELEASE);
+motor3.run(RELEASE);
+motor4.run(RELEASE);
+  }
+}
